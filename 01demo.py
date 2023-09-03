c13ba7ded2b3a625a8b9bb6c0b022a045cca634b
shanjiaomou <85399244+shanjiaomou@users.noreply.github.com>
2023-09-03 18:01:25 +0800

    更新保存BOM整合

--- 01demo.py
+++ 01demo.py
@@ -5,6 +5,7 @@ QQ交流群:788392508
 """
 import os
 import openpyxl
+import random
 from tkinter import filedialog
 from tkinter import messagebox
 from tkinter import *
@@ -12,20 +13,27 @@ from tkinter.ttk import *
 from typing import Dict
 
 #设置数组数据
-def array_set(array, value, row, col):
+def array_set(array, row, col, value):
     # 获取二维数组的行数和列数
-    rows = len(array)   #行
-    cols = len(array[0])#列
+    rows = len(array)
+    cols = len(array[0])
     # 如果插入位置超出数组范围，则扩展数组并使用空值补全
-    if row >= rows or col >= cols:
-        max_rows = max(row + 1, rows)
-        max_cols = max(col + 1, cols)
-        extended_array = [[None for _ in range(max_cols)] for _ in range(max_rows)]
-        # 将原数组的值复制到扩展后的数组中
+    if col >= cols:
+        for i in range(cols, col ):
+            for i in range(rows):
+                array[i].insert(col, None)
+        # 在指定位置插入新列
         for i in range(rows):
-            for j in range(cols):
-                extended_array[i][j] = array[i][j]
-        array = extended_array
+            array[i].insert(col, None)
+        # 获取二维数组的行数和列数
+        cols = len(array[0])
+    values=[None for _ in range(cols)]
+    # 如果插入位置超出数组范围，则扩展数组并使用空值补全
+    if row >= rows:
+        for i in range(rows, row ):
+            array.insert(i, values)
+        # 在指定位置插入新行
+        array.insert(row, values)
     # 在指定位置插入值
     array[row][col] = value
     return array
@@ -66,6 +74,19 @@ def insert_col(array, col_index):
     for i in range(rows):
         array[i].insert(col_index, None)
     return array   
+#插入剪切行
+def CutInsert_row(array,origin,finish):
+    # 将第origin行剪切插入第finish行前面
+    row_to_insert = array.pop(origin)  # 移除第origin行并保存
+    array.insert(finish, row_to_insert)  # 在第finish行前面插入移除的行
+    return array
+#插入剪切列
+def CutInsert_col(array,origin,finish):
+    # 将第origin列剪切插入第finish列前面
+    for row in array:
+        col_to_insert = row.pop(origin)  # 移除第origin列并保存
+        row.insert(finish, col_to_insert)  # 在第finish列前面插入移除的列
+    return array
 #获取行数
 def array_row(array):
     return len(array)
@@ -82,6 +103,10 @@ def print_log(self,srtbuf):
     self.tk_text_lm0p7c95.insert(END,srtbuf+"\r\n")
     self.tk_text_lm0p7c95.configure(state="disabled")
     self.tk_text_lm0p7c95.see(END)
+def Clear_log(self):
+    self.tk_text_lm0p7c95.configure(state="normal")
+    self.tk_text_lm0p7c95.delete("1.0", END)
+    self.tk_text_lm0p7c95.configure(state="disabled")
 def set_Prog(self,value,maximum):
     self.tk_progressbar_lm0p6l8w["maximum"]=maximum
     self.tk_progressbar_lm0p6l8w["value"]=value
@@ -89,10 +114,138 @@ def set_Prog(self,value,maximum):
 #用下面函数处理数据
 
 def Sheet_Handle(SheetBuf,array):
-    
-    pass
+    cell_value = SheetBuf['N3'].value                                       #获取主件料号（固定位置）
+    ArrayCol = array_col(array)                                             #获取数组列数
+    ArrayItem = ArrayCol-2                                                  #序号在表格中的列
+    ArrayDos = ArrayCol-1                                                   #用量在表格中的列
+    array = array_set(array,0,ArrayDos, cell_value)                         #写入主件料号
+    array = array_set(array,0,ArrayItem,cell_value)                         #写入主件料号    
+    BomMaxRow = SheetBuf.max_row                                            #获取物料工作表行数
+    OldItem = 0                                                             #初始化旧序号
+    StartAddr = 2                                                           #初始化缓存开始地址
+    EndAddr = 2                                                             #初始化缓存结束地址
+    MateAddr = 0                                                            #替代料尾地址
+    ArrayRow = array_row(array)                                             #获取数组行数
+    if(ArrayRow>2):                                                         #判断有数据了
+        SMNum = array_get(array,ArrayRow-1,0)                               #赋值替代料编号计数
+    else:                                                                   #否则
+        SMNum = 0                                                           #初始化替代料编号计数
+    SMbuf = 0                                                               #初始化替代料编号缓存
+    for sheet_i in range(4,BomMaxRow+1):                                    #遍历处理物料
+        if((SheetBuf.cell(sheet_i, 13).value=="P")                          
+           and(SheetBuf.cell(sheet_i, 22).value=="Y")):                     #判断物料有效，采购层次、认证情况
+            PartItem = int(SheetBuf.cell(sheet_i, 1).value)/10              #获取序号（固定位置），用于区分共用料
+            PartNO=SheetBuf.cell(sheet_i, 14).value                         #获取料号（固定位置）
+            Description=SheetBuf.cell(sheet_i, 17).value                    #获取描述（固定位置）
+            Dosage = SheetBuf.cell(sheet_i, 19).value                       #获取用量（固定位置）
+            ArrayRow = array_row(array)                                     #获取数组行数
+            if(OldItem!=PartItem):                                          #判断序号更新
+                if(EndAddr>StartAddr):                                      #上一个序号有缓存
+                    if(SMbuf!=0):                                           #判断替代料编号是否有缓存
+                        for array_i in range(2,ArrayRow):                   #遍历数组
+                            if(SMbuf == array_get(array,array_i,0)):        #查询相同缓存值
+                                MateAddr = array_i                          #更新最后缓存地址
+                        #for Insert_i in range(StartAddr,EndAddr):           #循环处理缓存
+                        #    array = array_set(array,Insert_i,0,SMbuf)       #赋值替代料编号
+                        for Insert_i in range(EndAddr-StartAddr):           #循环处理缓存
+                            array = CutInsert_row(array,ArrayRow-1,MateAddr+1)#把缓存插到替代料尾
+                            array = array_set(array,MateAddr+1,0,SMbuf)     #赋值替代料编号
+                    else:
+                        SMNum += 1                                          #更新替代料编号
+                        for Insert_i in range(StartAddr,EndAddr):           #循环处理缓存
+                            array = array_set(array,Insert_i,0,SMNum)       #赋值替代料编号
+                OldItem = PartItem                                          #更新序号
+                StartAddr = ArrayRow                                        #更新缓存开始地址
+                EndAddr = ArrayRow                                          #更新缓存结束地址
+                SMbuf = 0                                                   #更新替代料编号缓存
+            
+            for array_i in range(2,ArrayRow):                               #遍历数组
+                if(array_get(array,array_i,0)==PartNO):                     #索引料号是否有匹配
+                    array = array_set(array,array_i,ArrayItem,PartItem)     #写入序号
+                    array = array_set(array,array_i,ArrayDos, Dosage)       #写入用量
+                    SMbuf = array_get(array,array_i,0)                      #获取匹配替代料编号
+                    break                                                   #跳出本次遍历
+            else:                                                           #索引不到料号
+                array = array_set(array,EndAddr,1,          PartNO)         #写入料号
+                array = array_set(array,EndAddr,2,          Description)    #写入描述
+                array = array_set(array,EndAddr,ArrayItem,  PartItem)       #写入序号
+                array = array_set(array,EndAddr,ArrayDos,   Dosage)         #写入用量
+                EndAddr+=1                                                  #缓存结束地址+1
+    else:
+        if(EndAddr>StartAddr):                                      #上一个序号有缓存
+            if(SMbuf!=0):                                           #判断替代料编号是否有缓存
+                for array_i in range(2,ArrayRow):                   #遍历数组
+                    if(SMbuf == array_get(array,array_i,0)):        #查询相同缓存值
+                        MateAddr = array_i                          #更新最后缓存地址
+                ArrayRow = array_row(array)                         #获取数组行数
+                #for Insert_i in range(StartAddr,EndAddr):           #循环处理缓存
+                #    array = array_set(array,Insert_i,0,SMbuf)       #赋值替代料编号
+                for Insert_i in range(EndAddr-StartAddr):           #循环处理缓存
+                    array = CutInsert_row(array,ArrayRow-1,MateAddr+1)#把缓存插到替代料尾
+                    array = array_set(array,MateAddr+1,0,SMbuf)     #赋值替代料编号
+            else:
+                SMNum += 1                                          #更新替代料编号
+                for Insert_i in range(StartAddr,EndAddr):           #循环处理缓存
+                    array = array_set(array,Insert_i,0,SMNum)       #赋值替代料编号
+    return array
+
+def bom_format(array):
+    ArrayCol = array_col(array)
+    ModelCol = int((ArrayCol-3)/2)
+    for i in range(ModelCol):
+        array = CutInsert_col(array,(ArrayCol-2-i),0)
+    return array
+
+def bom_UseRatio(array):
+    ArrayCol = array_col(array)
+    ModelCol = int((ArrayCol-3)/2)                 #替代料位置
+    array_set(array, 0, ArrayCol, "现有库存")       #插入现有库存列
+    array=insert_col(array,ModelCol+3)
+    array_set(array, 0, ModelCol+3, "使用比例")     #插入使用比例列
+    array=insert_col(array,ModelCol+2)
+    array_set(array, 0, ModelCol+2, "迈腾代码")     #插入迈腾代码列
+    RatioLie = ModelCol+4                          #使用比例位置
+    MateStart = 0                                   #初始化索引行开始
+    ArrayRow = array_row(array)
+    OldItem = 0                                                             #初始化旧序号
+    for row_i in range(3,ArrayRow):#↓
+        PartItem = array_get(array,row_i,ModelCol)
+        if(OldItem!=PartItem):
+            array = array_set(array,row_i,RatioLie,"1")
+            MateStart = row_i
+            OldItem = PartItem
+        '''
+        else :
+            ArrayCol = array_col(array)
+            for col_i in range(ModelCol+5,ArrayCol):#→
+                if(array_get(array,row_i,col_i) is None):
+                    pass
+                else :
+                    for row_j in range(MateStart,row_i):#↓
+                        if(array_get(array,row_j,col_i) is None):
+                            pass
+                        else:
+                            break
+                    else:
+                        for col_j in range(ModelCol+5,ArrayCol):
+                            for row_j in range(MateStart,row_i):
+                                if(array_get(array,row_j,col_j) is None):
+                                    pass
+                                else:
+                                    array = array_set(array,)
+                                    break
+                            
+                        break
+                    '''
+        
+    return array 
+
 
 def main_app(self):
+    TotalArray = [[["替代料","小米料号","物料描述"],[None,None,None]],
+                  [["替代料","小米料号","物料描述"],[None,None,None]],
+                  [["替代料","小米料号","物料描述"],[None,None,None]]]
+    Clear_log(self)
     # 获取文件夹中的所有文件和文件夹
     folder_path = self.tk_input_lm0omywa.get()
     try:
@@ -103,25 +256,118 @@ def main_app(self):
     file_len = len(file_list)
     
     for file_count in range(file_len):
-        set_Prog(self,file_count+1,file_len)
+        set_Prog(self,file_count+1,file_len)#更新进度条
         file_name = file_list[file_count]
         print_label(self,"导入BOM:"+str(file_count+1)+"/"+str(file_len))
         print_log(self,"导入BOM:"+file_name)
         Model_Last=file_name.find(".xlsx")
-        if(("BOM清单_L95"!=file_name[0:9])and(0>Model_Last)):
+        if(("BOM清单_L95"!=file_name[0:9])or(0>Model_Last)):
             print_log(self," └文件名错误")
-            return
-        BomWorkBook=openpyxl(folder_path+"/"+file_name,data_only=True)
-        
-        
-        
-        
+            win.update()#更新界面
+            continue
+        win.update()#更新界面
+        # 产品型号
         Model_Name = file_name[20:Model_Last]
-        
-        
-        
-
-
+        # 加载Excel文档
+        BomWorkBook=openpyxl.load_workbook(folder_path+"/"+file_name,data_only=True)
+        # 获取所有工作表的名称
+        BomSheetNames = BomWorkBook.sheetnames
+        # 遍历输出每个工作表的名称
+        for BomSheetName in BomSheetNames:
+            BomSheet = BomWorkBook[BomSheetName]
+            if(0<BomSheetName.find("PACKAGING")):
+                ArrayCol      = array_col(TotalArray[2])
+                TotalArray[2] = array_set(TotalArray[2],1,ArrayCol,  Model_Name)
+                TotalArray[2] = array_set(TotalArray[2],1,ArrayCol+1,Model_Name)
+                TotalArray[2] = Sheet_Handle(BomSheet,TotalArray[2])
+            elif(0<BomSheetName.find("PCBA")):
+                ArrayCol      = array_col(TotalArray[0])
+                TotalArray[0] = array_set(TotalArray[0],1,ArrayCol,  Model_Name)
+                TotalArray[0] = array_set(TotalArray[0],1,ArrayCol+1,Model_Name)                
+                TotalArray[0] = Sheet_Handle(BomSheet,TotalArray[0])
+            elif(0<BomSheetName.find("FA")):
+                ArrayCol      = array_col(TotalArray[1])
+                TotalArray[1] = array_set(TotalArray[1],1,ArrayCol,  Model_Name)
+                TotalArray[1] = array_set(TotalArray[1],1,ArrayCol+1,Model_Name)
+                TotalArray[1] = Sheet_Handle(BomSheet,TotalArray[1])
+        #释放处理完的工作簿
+        BomWorkBook.close()
+    
+    print_label(self,"调整格式")
+    print_log(self,"调整格式")
+    set_Prog(self,0,3)#更新进度条
+    win.update()#更新界面
+    #调整数组位置
+    for i in range(3):
+        set_Prog(self,i+1,3)#更新进度条
+        win.update()#更新界面
+        TotalArray[i]=bom_format(TotalArray[i])
+    TotalArrayLen = [array_row(TotalArray[0]),array_row(TotalArray[1]),array_row(TotalArray[2])]
+    ProgLen = TotalArrayLen[0]+TotalArrayLen[1]+TotalArrayLen[2]
+    set_Prog(self,0,ProgLen)#更新进度条
+    print_label(self,"存储BOM整合清单")
+    print_log(self,"存储BOM整合清单")
+    win.update()#更新界面
+    # 创建一个新的Excel文件
+    OutWorkBook = openpyxl.Workbook()
+    # 选择一个工作表
+    OutSheet = (OutWorkBook.create_sheet("电子料",0),
+               OutWorkBook.create_sheet("结构料",1),
+               OutWorkBook.create_sheet("包材",2))
+    for i in range(3):
+        for j in range(TotalArrayLen[i]):
+            if i == 0:
+                ProgBar = j
+            elif i == 1:
+                ProgBar = TotalArrayLen[0]+j
+            else :
+                ProgBar = TotalArrayLen[0]+TotalArrayLen[0]+j
+            set_Prog(self,ProgBar,ProgLen)#更新进度条
+            win.update()#更新界面
+            OutSheet[i].append(TotalArray[i][j])
+    try:
+        SaveName=".\BOM整合清单.xlsx"
+        OutWorkBook.save(SaveName)
+    except:
+        SaveName=".\BOM整合清单"+str(random.randint(0,65535))+".xlsx"
+        OutWorkBook.save(SaveName)
+    OutWorkBook.close
+    print_label(self,"保存："+SaveName)
+    print_log(self,"保存："+SaveName)
+    win.update()#更新界面
+    
+    # 创建一个新的Excel文件
+    OutWorkBook = openpyxl.Workbook()
+    # 选择一个工作表
+    OutSheet = (OutWorkBook.create_sheet("电子料",0),
+               OutWorkBook.create_sheet("结构料",1),
+               OutWorkBook.create_sheet("包材",2))    
+    for i in range(3):
+        print_label(self,"处理使用比例："+str(i+1)+"/3")
+        print_log(self,"处理使用比例："+str(i+1)+"/3")
+        win.update()#更新界面
+        TotalArray[i] = bom_UseRatio(TotalArray[i])
+        for j in range(TotalArrayLen[i]):
+            if i == 0:
+                ProgBar = j
+            elif i == 1:
+                ProgBar = TotalArrayLen[0]+j
+            else :
+                ProgBar = TotalArrayLen[0]+TotalArrayLen[0]+j
+            set_Prog(self,ProgBar,ProgLen)#更新进度条
+            win.update()#更新界面
+            OutSheet[i].append(TotalArray[i][j])
+    try:
+        SaveName=".\BOM使用比例清单.xlsx"
+        OutWorkBook.save(SaveName)
+    except:
+        SaveName=".\BOM使用比例清单"+str(random.randint(0,65535))+".xlsx"
+        OutWorkBook.save(SaveName)
+    OutWorkBook.close
+    print_label(self,"保存："+SaveName)
+    print_log(self,"保存："+SaveName)
+    win.update()#更新界面
+    
 class WinGUI(Tk):
     def __init__(self):
         super().__init__()
@@ -198,15 +444,21 @@ class Win(WinGUI):
     def __init__(self):
         super().__init__()
         self.__event_bind()
-    def OpenFolderEvent(self,evt):
-        folder_path = filedialog.askdirectory(initialdir="./")  # 打开文件夹选择对话框，设置初始目录为.py文件所在的位置
-        self.tk_input_lm0omywa.delete(0, END)  # 清空输入框内容
-        self.tk_input_lm0omywa.insert(END, folder_path)  # 将选择的目录路径填入输入框
-    def StartProcessEvent(self,evt):
+    def OpenFolderEvent(self):#(self,evt):
+        # 打开文件夹选择对话框，设置初始目录为.py文件所在的位置
+        folder_path = filedialog.askdirectory(initialdir=self.tk_input_lm0omywa.get())  
+        if(folder_path!=""):
+            self.tk_input_lm0omywa.delete(0, END)  # 清空输入框内容
+            self.tk_input_lm0omywa.insert(END, folder_path)  # 将选择的目录路径填入输入框
+    def StartProcessEvent(self):#(self,evt):
+        self.tk_button_lm0onmq9.config(state=DISABLED)
         main_app(self)
+        self.tk_button_lm0onmq9.config(state=NORMAL)
     def __event_bind(self):
-        self.tk_button_lm0on4cw.bind('<Button>',self.OpenFolderEvent)
-        self.tk_button_lm0onmq9.bind('<Button>',self.StartProcessEvent)
+        #self.tk_button_lm0on4cw.bind('<Button>',self.OpenFolderEvent)
+        #self.tk_button_lm0onmq9.bind('<Button>',self.StartProcessEvent)
+        self.tk_button_lm0on4cw["command"]=self.OpenFolderEvent
+        self.tk_button_lm0onmq9["command"]=self.StartProcessEvent
         pass
 if __name__ == "__main__":
     win = Win()
